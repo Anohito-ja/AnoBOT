da586115dce169225254cbf7d96e6b2e38dc68b8
anohitoLPAT <anohito4137@gmail.com>
2025-08-28 09:18:42 +0900

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { Events, EmbedBuilder } = require('discord.js');
 
 module.exports = {
@@ -15,22 +14,4 @@ module.exports = {
       }
     }
   },
-=======
-const { Events, EmbedBuilder } = require('discord.js');
-
-module.exports = {
-  name: Events.GuildMemberAdd,
-  async execute(member, client) {
-    const logChannelId = client.settings.logChannel;
-    if (logChannelId) {
-      const logChannel = member.guild.channels.cache.get(logChannelId);
-      if (logChannel) {
-        const welcomeEmbed = new EmbedBuilder()
-          .setColor(0x00ff00)
-          .setDescription(`✅ ${member.user.tag} がサーバーに参加しました！`);
-        logChannel.send({ embeds: [welcomeEmbed] });
-      }
-    }
-  },
->>>>>>> 847512c7e09a4c27175b8ed36990db4821422739
 };
