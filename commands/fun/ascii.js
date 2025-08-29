21d53cd88546358f9bddafe1d00785afe828f896
anohitoLPAT <anohito4137@gmail.com>
2025-08-29 19:04:32 +0900

    Update ascii.js

--- commands/fun/ascii.js
+++ commands/fun/ascii.js
@@ -1,65 +1,27 @@
 const { SlashCommandBuilder } = require('discord.js');
-const figlet = require('figlet');
+const figlet = require('figlet'); // figletライブラリをインストールする必要があります
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ascii')
-    .setDescription('入力されたテキストをアスキーアートに変換します。')
+    .setDescription('テキストをASCIIアートに変換します。')
     .addStringOption(option =>
       option.setName('text')
-        .setDescription('アスキーアートに変換するテキスト')
+        .setDescription('ASCIIアートに変換するテキスト')
         .setRequired(true)),
   async execute(interaction) {
     const text = interaction.options.getString('text');
 
-    figlet.text(text, {
-      font: 'Standard', // 他のフォントも利用可能です
-      horizontalLayout: 'default',
-      verticalLayout: 'default'
-    }, function(err, data) {
+    figlet.text(text, (err, data) => {
       if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
+        console.error('ASCIIアートの生成中にエラーが発生しました:', err);
+        return interaction.reply('エラーが発生しました。もう一度お試しください。');
+      }
+
+      if (data.length > 2000) {
+        return interaction.reply('生成されたASCIIアートが長すぎます。もっと短いテキストを試してください。');
       }
       
-      if (data.length > 2000) {
-        return interaction.reply({ content: 'テキストが長すぎます。', ephemeral: true });
-      }
-
-      interaction.reply({ content: `\`\`\`\n${data}\n\`\`\`` });
-    });
-  },
-
-const { SlashCommandBuilder } = require('discord.js');
-const figlet = require('figlet');
-
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('ascii')
-    .setDescription('入力されたテキストをアスキーアートに変換します。')
-    .addStringOption(option =>
-      option.setName('text')
-        .setDescription('アスキーアートに変換するテキスト')
-        .setRequired(true)),
-  async execute(interaction) {
-    const text = interaction.options.getString('text');
-
-    figlet.text(text, {
-      font: 'Standard', // 他のフォントも利用可能です
-      horizontalLayout: 'default',
-      verticalLayout: 'default'
-    }, function(err, data) {
-      if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
-      }
-      
-      if (data.length > 2000) {
-        return interaction.reply({ content: 'テキストが長すぎます。', ephemeral: true });
-      }
-
       interaction.reply({ content: `\`\`\`\n${data}\n\`\`\`` });
     });
   },
